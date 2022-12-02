78680a91416cfebd6f1bc63f899100fa0639eb31
piekvossen <piekvossen@gmail.com>
2022-12-02 09:53:12 +0100

    - new format emissor

--- examples/likelihood_evaluation_of_emissor_scenario.py
+++ examples/likelihood_evaluation_of_emissor_scenario.py
@@ -5,26 +5,21 @@ from pathlib import Path
 
 SCENARIOS_FOLDER = Path("/Users/piek/Desktop/t-MA-Combots-2022/assignments/interaction1")
 
-# evaluator_ft = LikelihoodEvaluator(model_path_mlm='adamlin/usr-topicalchat-roberta_ft', max_context=300,
+# evaluator = LikelihoodEvaluator(model_path_mlm='adamlin/usr-topicalchat-roberta_ft', max_context=300,
 #                                 len_top_tokens=20)
+#METRICS = "MLM llh"
 
-# evaluator_ctx = USR_DialogRetrieval_Evaluator(model_path_ctx='adamlin/usr-topicalchat-ctx', context_type="ctx", max_context=300,
+# evaluator = USR_DialogRetrieval_Evaluator(model_path_ctx='adamlin/usr-topicalchat-ctx', context_type="ctx", max_context=300,
 #                                 len_top_tokens=20)
-
-evaluator_uk = USR_DialogRetrieval_Evaluator(model_path_ctx='adamlin/usr-topicalchat-uk',context_type="fct",  max_context=300,
+#METRICS = "Ctx Score"
+evaluator = USR_DialogRetrieval_Evaluator(model_path_ctx='adamlin/usr-topicalchat-uk',context_type="fct",  max_context=300,
                                  len_top_tokens=20)
-
+METRICS = "Ctx Score"
 
 
 for path in os.listdir(SCENARIOS_FOLDER):
     if os.path.isdir(os.path.join(SCENARIOS_FOLDER, path)):
-        # evaluator_ft.evaluate_conversation(scenario_folder=SCENARIOS_FOLDER,
-        #                                 scenario_id=path,
-        #                                 metrics_to_plot=['MLM llh'])
-        # evaluator_ctx.evaluate_conversation(scenario_folder=SCENARIOS_FOLDER,
-        #                                 scenario_id=path,
-        #                                 metrics_to_plot=['Ctx Score'])
-        evaluator_uk.evaluate_conversation(scenario_folder=SCENARIOS_FOLDER,
-                                        scenario_id=path,
-                                        metrics_to_plot=['Ctx Score'])
+         evaluator.evaluate_conversation(scenario_folder=SCENARIOS_FOLDER,
+                                         scenario_id=path,
+                                         metrics_to_plot=[METRICS])
 
