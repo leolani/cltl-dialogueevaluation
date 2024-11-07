f9bb62288879c2ead9363612dca0aace15233452
piekvossen <piekvossen@gmail.com>
2024-11-07 21:26:13 +0100

    using datasets instead of evaluate, implemented blue, rouge, meteor and bertscore for reference evaluations

--- examples/statistical_evaluation_of_emissor_scenario.py
+++ examples/statistical_evaluation_of_emissor_scenario.py
@@ -1,8 +1,6 @@
 from cltl.dialogue_evaluation.statistical_evaluation import StatisticalEvaluator
 import os
 
-
-
 def main():
     evaluator = StatisticalEvaluator()
     emissor_path = '/Users/piek/Desktop/t-MA-Combots-2024/code/ma-communicative-robots/emissor_chat/emissor'
@@ -31,22 +29,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-#
-# for scenario in os.listdir(emissor_path):
-#     if not scenario.startswith("."):
-#         scenario_path = os.path.abspath(scenario)
-#         print('Path', scenario_path)
-#         print('Scenario', scenario)
-#         has_scenario, has_text, has_image, has_rdf = evaluator.check_scenario_data(scenario_path, scenario)
-#         check_message = "Scenario folder:" + scenario_path + "\n"
-#         check_message += "\tScenario JSON:" + str(has_scenario) + "\n"
-#         check_message += "\tText JSON:" + str(has_text) + "\n"
-#         check_message += "\tImage JSON:" + str(has_image) + "\n"
-#         check_message += "\tRDF :" + str(has_rdf) + "\n"
-#         print(check_message)
-#         if not has_scenario:
-#             print("No scenario JSON found. Skipping:", scenario_path)
-#         elif not has_text:
-#             print("No text JSON found. Skipping:", scenario_path)
-#         else:
-#             evaluator.analyse_interaction_json(scenario_path, scenario)
