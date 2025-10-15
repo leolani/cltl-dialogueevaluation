75d1cd34a149d7724d67f811f3401f719b4f5d9b
piekvossen <piekvossen@gmail.com>
2025-10-15 22:07:35 +0200

    Correlation functions repaired Interaction plot enhanced with image signals and face emotions

--- setup.py
+++ setup.py
@@ -23,29 +23,29 @@ setup(
     packages=find_namespace_packages(include=['cltl.*'], where='src'),
     package_data={'cltl.dialogue_evaluation': ['data/*']},
     python_requires='>=3.10',
-    install_requires=[
-    'numpy==1.26.4',
-    'pandas==2.2.3',
-    'scipy==1.15.2',
-    'seaborn==0.13.2',
-    'transformers==4.51.3',
-    'emissor==0.0.dev4',  # Note this is a slightly different version from your setup.py
-    'torch==2.2.2',
-    'valuate==0.4.3',
-    'accelerate==1.6.0',
-    'bert-score==0.3.13',
-    'bleach==6.2.0',
-    'chardet==5.2.0',
-    'click==8.1.8',
-    'datasets==3.5.1',
-    'fqdn==1.5.1',
-    'nltk==3.9.1',
-    'rouge-score==0.1.2',
-    'spacy==3.8.5',
-    'tqdm==4.67.1',
-    'openpyxl==3.1.5',
-    'cltl.combot==1.1.dev1'
-]
+#     install_requires=[
+#     'numpy==1.26.4',
+#     'pandas==2.2.3',
+#     'scipy==1.15.2',
+#     'seaborn==0.13.2',
+#     'transformers==4.51.3',
+#     'emissor==0.0.dev4',  # Note this is a slightly different version from your setup.py
+#     'torch==2.2.2',
+#     'valuate==0.4.3',
+#     'accelerate==1.6.0',
+#     'bert-score==0.3.13',
+#     'bleach==6.2.0',
+#     'chardet==5.2.0',
+#     'click==8.1.8',
+#     'datasets==3.5.1',
+#     'fqdn==1.5.1',
+#     'nltk==3.9.1',
+#     'rouge-score==0.1.2',
+#     'spacy==3.8.5',
+#     'tqdm==4.67.1',
+#     'openpyxl==3.1.5',
+#     'cltl.combot==1.1.dev1'
+# ]
     ,
     setup_requires=['flake8']
 )
